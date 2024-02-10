f198cc757c1607ba3d307add515601df0ed5121b
rragul <r.ragul0598@gmail.com>
2024-02-10 10:29:20 +0530

    initial commit

---
+++ src/main/java/com/skinclear/skinclearbackend/SkinclearBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skinclear.skinclearbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinclearBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkinclearBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skinclear/skinclearbackend/SkinclearBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skinclear.skinclearbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinclearBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
