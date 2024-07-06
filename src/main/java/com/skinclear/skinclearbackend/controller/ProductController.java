a2c7fd3c20e383417f347577b4aee89f9965b4fb
rragul <r.ragul0598@gmail.com>
2024-07-06 08:36:31 +0530

    Update: refactor product related code

--- src/main/java/com/skinclear/skinclearbackend/controller/ProductController.java
+++ src/main/java/com/skinclear/skinclearbackend/controller/ProductController.java
@@ -1,16 +1,23 @@
 package com.skinclear.skinclearbackend.controller;
 
+import com.skinclear.skinclearbackend.entity.Product;
+import com.skinclear.skinclearbackend.resource.GeneralResponse;
 import com.skinclear.skinclearbackend.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/product")
 public class ProductController extends AbstractController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
     private  final ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -18,7 +25,15 @@ public class ProductController extends AbstractController {
     }
 
     @GetMapping
-    public ResponseEntity<Object> getAllProducts(){
-        return sendSuccessResponse(productService.getAllProduct());
+    public ResponseEntity<GeneralResponse> getAllProducts(){
+        logger.info("request - getAllProducts | (URL: /api/v1/product) | (Method: GET)");
+        List<Product> allProducts = productService.getAllProduct();
+        logger.info("response - getAllProducts | (URL: /api/v1/product) | (Method: GET) | (status: 200)");
+        return ResponseEntity.ok(
+                GeneralResponse.builder()
+                        .success(true)
+                        .data(allProducts)
+                        .build()
+        );
     }
 }
