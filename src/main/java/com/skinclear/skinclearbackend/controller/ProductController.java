8173df062d9242da09261f52d349efa0b6c81a34
Kathiramalai-Thilakshan <thilakshan@techserw.com>
2024-07-08 23:18:20 +0530

    Add:update product controller update ul

--- src/main/java/com/skinclear/skinclearbackend/controller/ProductController.java
+++ src/main/java/com/skinclear/skinclearbackend/controller/ProductController.java
@@ -100,7 +100,7 @@ public class ProductController extends AbstractController {
         );
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("/update/{id}")
     public ResponseEntity<GeneralResponse> updateProduct(@PathVariable Long id, @RequestBody ProductDTO product){
         try {
             logger.info("request - updateProduct | (URL: /api/v1/product/{}) | (Method: PUT)", id);
