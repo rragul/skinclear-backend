29215d13222c557f3231eed50a7c6857d259c3af
rragul <r.ragul0598@gmail.com>
2024-07-19 18:52:57 +0530

    Update: getProductsByFilter

--- src/main/java/com/skinclear/skinclearbackend/service/ProductService.java
+++ src/main/java/com/skinclear/skinclearbackend/service/ProductService.java
@@ -172,6 +172,9 @@ public class ProductService {
     }
 
     public Page<Product> getProductsByFilter(String subCategory, String preference, String benefits, String type, String ingredient, String brand, int page, int size) {
+        if (subCategory == null && preference == null && benefits == null && type == null && ingredient == null && brand == null) {
+            return productRepository.findAll(PageRequest.of(page, size));
+        }
         return productRepository.findProductsByFilter(subCategory, preference, benefits, type, ingredient, brand, PageRequest.of(page, size));
     }
 
