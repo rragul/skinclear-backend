a49c90ba1d8986ca649f1b30b7b53cc513de0beb
rragul <r.ragul0598@gmail.com>
2024-08-04 10:48:43 +0530

    Update: brand controller

--- src/main/java/com/skinclear/skinclearbackend/controller/BrandController.java
+++ src/main/java/com/skinclear/skinclearbackend/controller/BrandController.java
@@ -6,7 +6,6 @@ import com.skinclear.skinclearbackend.resource.GeneralResponse;
 import com.skinclear.skinclearbackend.service.BrandService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
