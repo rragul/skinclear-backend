39c969f17c91690f217a8b41e1a12ea02bcc278a
rragul <r.ragul0598@gmail.com>
2024-08-06 17:44:33 +0530

    Update: username as not unique

--- src/main/java/com/skinclear/skinclearbackend/entity/User.java
+++ src/main/java/com/skinclear/skinclearbackend/entity/User.java
@@ -20,7 +20,7 @@ public class User {
     private String firstName;
     @Column(nullable = false)
     private String lastName;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String userName;
     @Column(nullable = false, unique = true)
     private String email;
