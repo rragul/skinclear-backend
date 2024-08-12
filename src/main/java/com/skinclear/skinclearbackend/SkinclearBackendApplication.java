1e07cc5270488e2d17bd8d60fcda9082f0061807
Ragul <62707278+rragul@users.noreply.github.com>
2024-08-12 18:56:15 +0530

    ADD: corsFilter

--- src/main/java/com/skinclear/skinclearbackend/SkinclearBackendApplication.java
+++ src/main/java/com/skinclear/skinclearbackend/SkinclearBackendApplication.java
@@ -2,12 +2,28 @@ package com.skinclear.skinclearbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SkinclearBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SkinclearBackendApplication.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("https://localhost:3000","http://localhost:3000","https://d2azwxcl0602xy.cloudfront.net/","https://d2azwxcl0602xy.cloudfront.net"));
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
